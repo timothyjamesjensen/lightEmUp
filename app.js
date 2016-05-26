5864ebddd992ae90915d7f7371b14711960ed424
= <timothyjamesjensen@gmail.com>
2016-05-26 01:25:58 -0700

    added light switch function

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var app = angular.module('app',[]);
 
-app.controller('app-ctrl', function($scope) {
+app.controller('app-ctrl', function($scope, $http) {
+
+    $scope.lightSwitch = function() {
+        var req = {
+            method: 'POST',
+            url: 'http://somewhereoutthere/light'
+        };
+        $http(req);
+        console.log("on");
+    };
 
 });
